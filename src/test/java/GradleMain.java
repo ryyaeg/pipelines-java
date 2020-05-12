7131391c7287e1c5aabe816c925cbe01bf902150
ryyaeg <59249423+ryyaeg@users.noreply.github.com>
2020-05-12 15:31:20 +0900

    Update GradleMain.java

--- src/test/java/GradleMain.java
+++ src/test/java/GradleMain.java
@@ -1,7 +1,7 @@
 package sample.gradle;
 
 public class GradleMain {
-    public static int add(int a, int b) {
-        return a + b;
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle!!");
     }
 }
